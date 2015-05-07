9df3ea64b3242b26bc252b29243e6a7608734241
Afonso F. Garcia <afonsogarcia@icloud.com>
2015-05-07 14:22:05 -0700

    Test

--- swift/common/middleware/adaptive_decompression.py
+++ swift/common/middleware/adaptive_decompression.py
@@ -30,7 +30,7 @@ class AdaptiveDecompressionMiddleware(object):
 		if not path in self.__class__.storage:
 			self.__class__.storage[path] = {}
 		
-		#body = env['wsgi.input'].read()
+		body = req.body_file.read()
 		
 		# Inflage the chunk
 		#chunk = zlib.decompress(body)
