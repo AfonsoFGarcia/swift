c4135efa325e46fad5b6563e9f960f4fc51815ea
Afonso F. Garcia <afonsogarcia@icloud.com>
2015-05-08 01:59:46 -0700

    Debug

--- swift/common/middleware/adaptive_decompression.py
+++ swift/common/middleware/adaptive_decompression.py
@@ -34,8 +34,7 @@ class AdaptiveDecompressionMiddleware(object):
 		if not path in self.__class__.storage:
 			self.__class__.storage[path] = {}
 		
-		self.logger.info(env['wsgi.input'].read())
-		self.logger.info(env['wsgi.input'].read())
+		self.logger.info(env['wsgi.input'].read(req.message_length))
 		
 		#body = env['wsgi.input'].read()
 		
